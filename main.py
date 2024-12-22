4f982cf3f8c510dc602e214f773b1d652fe52c59
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-12-22 07:43:17 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+empty_list = []
+print(empty_list)
+
+numbers = 1,2,3,4,5
+print(numbers)
+
+triplets = (1,2,3)*3
+print(triplets)
+
+aList = 100, 200, 300, 400, 500
+aList = aList[::-1]
+print(aList,"/n")
